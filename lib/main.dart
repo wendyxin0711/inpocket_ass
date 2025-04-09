fde8c4391a363cd04a1ba6e6122ab7ca877b015b
wendyxin0711 <wendycwx-wp22@student.tarc.edu.my>
2025-04-09 23:11:11 +0800

    update title name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'inPocket1',
+      title: 'inPocket',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
